ae900fe8e6e251e73c3007821e1d7ecb0a0b0f32
Stephanie Renaud <stephanie.renaud39@gmail.com>
2018-07-13 21:34:03 +0200

    classe avec genericite

--- SR/src/com/s2rltx/projet/foodshuflle/model/Ingredient.java
+++ SR/src/com/s2rltx/projet/foodshuflle/model/Ingredient.java
@@ -7,9 +7,9 @@ package com.s2rltx.projet.foodshuflle.model;
  * @author Stéphanie
  *
  */
-public class Ingredient {
+public class Ingredient extends DBBaseItem{
 
-	private int idIng;
+	/*private int idIng;
 	private String nom;
 	private String unite;
 	private String groupe;
@@ -55,6 +55,40 @@ public class Ingredient {
 		this.kcal = kcal;
 	}
 	
+	public Ingredient() {
+		
+	}*/
 	
+	private static final String TABLE = "Ingredient";
+	private static final String ID = "id";
+	private static final String[][] FIELDS = { 
+			{ "nom", "VARCHAR(255)" }, 
+			{ "unite", "VARCHAR(5)" }, {"groupe", "VARCHAR(100)"},{"kcal", "INT(11)"} 
+			};
 	
+	public Ingredient() {
+		super(TABLE, ID);
+	}
+	
+	@Override
+	public String getSchema() {
+		return "CREATE TABLE " + TABLE + " ("  
+				+ ID + " INT AUTO_INCREMENT PRIMARY KEY NOT NULL," 
+				+ FIELDS[0][0] + " " + FIELDS[0][1] + ","
+				+ FIELDS[1][0] + " " + FIELDS[1][1] + ","
+				+ FIELDS[2][0] + " " + FIELDS[2][1] + ","
+				+ FIELDS[3][0] + " " + FIELDS[3][1] + ""
+				+ ")";
+	}
+
+	@Override
+	public String getFields() {
+		String result = "";
+		int i = 0;
+		for (; i < FIELDS.length-1; i++) {
+			result += FIELDS[i][0] + ",";
+		}
+		result += FIELDS[i][0];
+		return result;
+	}
 }
--- SR/src/com/s2rltx/projet/foodshuflle/model/NomDePlat.java
+++ SR/src/com/s2rltx/projet/foodshuflle/model/NomDePlat.java
@@ -9,7 +9,7 @@ package com.s2rltx.projet.foodshuflle.model;
  */
 public class NomDePlat {
 
-	int idNDP;
+	/*int idNDP;
 	String nomDePlat;
 	String regime;
 	
@@ -39,7 +39,40 @@ public class NomDePlat {
 		this.nomDePlat = nomDePlat;
 		this.regime = regime;
 	}
+	*/
+	public NomDePlat() {
+		
+	}
+	private static final String TABLE = "NomDePlat";
+	private static final String ID = "id";
+	private static final String[][] FIELDS = { 
+			{ "nom", "VARCHAR(255)"}, {"regime", "VARCHAR(255)"} 
+			};
 	
+	public NomDePlat() {
+		super(TABLE, ID);
+	}
 	
+	@Override
+	public String getSchema() {
+		return "CREATE TABLE " + TABLE + " ("  
+				+ ID + " INT AUTO_INCREMENT PRIMARY KEY NOT NULL," 
+				+ FIELDS[0][0] + " " + FIELDS[0][1] + ","
+				+ FIELDS[1][0] + " " + FIELDS[1][1] + ""
+				+ ")";
+	}
+	
+
+	@Override
+	public String getFields() {
+		String result = "";
+		int i = 0;
+		for (; i < FIELDS.length-1; i++) {
+			result += FIELDS[i][0] + ",";
+		}
+		result += FIELDS[i][0];
+		return result;
+	}
+
 	
 }
