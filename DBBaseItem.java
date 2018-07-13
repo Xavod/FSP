feea2d74d205b232f201472abab6d74bc299021b
Xavod <39615872+Xavod@users.noreply.github.com>
2018-07-13 14:41:31 +0200

    Add files via upload

---
+++ DBBaseItem.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.sr2ltx.algotojava.projetFoodShuffle;
+
+import java.sql.ResultSet;
+
+/**
+ * @author Apprenant
+ *
+ */
+public abstract class DBBaseItem {
+	
+	public String TABLE;
+	public String ID;
+	
+	private int id;
+	private Object item;
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTABLE() {
+		return TABLE;
+	}
+
+	public String getID() {
+		return ID;
+	}
+	
+	public Object getItem() {
+		return item;
+	}
+
+	public void setItem(Object item) {
+		this.item = item;
+	}
+
+	public DBBaseItem(String table, String id){
+		this.TABLE = table;
+		this.ID = id;
+	}
+	
+	public abstract DBBaseItem parseIn(ResultSet rs);
+	
+	public abstract String getSchema();
+	
+	public abstract String parseOut(Object item);
+	
+	public abstract String getContraintes();
+
+	public abstract String getFields();
+}
