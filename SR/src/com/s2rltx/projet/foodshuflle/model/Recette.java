ba039a197dd6b13ef0003c892cb4dce6cad0d6fb
Stephanie Renaud <stephanie.renaud39@gmail.com>
2018-07-17 20:49:04 +0200

    Fichier fxml

--- SR/src/com/s2rltx/projet/foodshuflle/model/Recette.java
+++ SR/src/com/s2rltx/projet/foodshuflle/model/Recette.java
@@ -3,85 +3,14 @@
  */
 package com.s2rltx.projet.foodshuflle.model;
 
+import com.s2rltx.projet.foodshuflle.manager.DBBaseItem;
+
 /**
  * @author Stéphanie
  *
  */
-public class Recette {
+public abstract class Recette extends DBBaseItem {
 
-	/*int idR;
-	NomDePlat idP;
-	int nbrPersonne;
-	int tpsPrepa;
-	int tpsCuisson;
-	Ingredient idIng;
-	int quantite;
-	Ingredient unite;
-	
-	public int getIdR() {
-		return idR;
-	}
-	public void setIdR(int idR) {
-		this.idR = idR;
-	}
-	public NomDePlat getIdP() {
-		return idP;
-	}
-	public void setIdP(NomDePlat idP) {
-		this.idP = idP;
-	}
-	public int getNbrPersonne() {
-		return nbrPersonne;
-	}
-	public void setNbrPersonne(int nbrPersonne) {
-		this.nbrPersonne = nbrPersonne;
-	}
-	public int getTpsPrepa() {
-		return tpsPrepa;
-	}
-	public void setTpsPrepa(int tpsPrepa) {
-		this.tpsPrepa = tpsPrepa;
-	}
-	public int getTpsCuisson() {
-		return tpsCuisson;
-	}
-	public void setTpsCuisson(int tpsCuisson) {
-		this.tpsCuisson = tpsCuisson;
-	}
-	public Ingredient getIdIng() {
-		return idIng;
-	}
-	public void setIdIng(Ingredient idIng) {
-		this.idIng = idIng;
-	}
-	public int getQuantite() {
-		return quantite;
-	}
-	public void setQuantite(int quantite) {
-		this.quantite = quantite;
-	}
-	public Ingredient getUnite() {
-		return unite;
-	}
-	public void setUnite(Ingredient unite) {
-		this.unite = unite;
-	}
-	
-	public Recette(int idR, NomDePlat idP, int nbrPersonne, int tpsPrepa, int tpsCuisson, Ingredient idIng,
-			int quantite, Ingredient unite) {
-		super();
-		this.idR = idR;
-		this.idP = idP;
-		this.nbrPersonne = nbrPersonne;
-		this.tpsPrepa = tpsPrepa;
-		this.tpsCuisson = tpsCuisson;
-		this.idIng = idIng;
-		this.quantite = quantite;
-		this.unite = unite;
-	}*/
-	 public Recette () {
-		 
-	 }
 	private static final String TABLE = "Recette";
 	private static final String ID = "id";
 	private static final String[][] FIELDS = { 
@@ -105,13 +34,8 @@ public class Recette {
 				+ FIELDS[5][0] + " " + FIELDS[5][1] + ""
 				+ ")";
 	}
-	@Override
-	public getContraints() {
-		System.out.println("FOREIGN KEY (P_id) references nomDePlat(id)");
-				
-	}
-	
-	
+			
+
 	@Override
 	public String getFields() {
 		String result = "";
